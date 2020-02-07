412fd0aec2a08833a7cea2791afe0ea0e3a3ea2e
quaresmateo <teoian.giovane@gmail.com>
2020-02-07 14:49:13 -0300

    Cria migration da tabela de Eventos

---
+++ database/migrations/1581006169235_event_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class EventSchema extends Schema {
+  up() {
+    this.create('events', table => {
+      table.increments()
+      table.string('owner').notNullable()
+      table.string('email').notNullable()
+      table.string('title').notNullable()
+      table.text('description').defaultTo('')
+      table.datetime('start', { presicion: 6 }).notNullable()
+      table.datetime('end', { precision: 6 }).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('events')
+  }
+}
+
+module.exports = EventSchema
