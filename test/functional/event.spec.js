d1346b8188817beb2a148679c822621cbe934faa
quaresmateo <teoian.giovane@gmail.com>
2020-04-02 10:50:42 -0300

    Cria teste funcional para cadastro de eventos

--- test/functional/event.spec.js
+++ test/functional/event.spec.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const { test, trait } = use('Test/Suite')('Event')
-const Event = use('App/Models/Event')
 const Factory = use('Factory')
 
 trait('Test/ApiClient')
+trait('Auth/Client')
 trait('DatabaseTransactions')
 
-test('can create a event', async ({ client }) => {
+test('authorized user can create a event', async ({ client }) => {
+  const user = await Factory.model('App/Models/User').create()
   const response = await client
     .post('/events')
     .send({
@@ -18,17 +19,49 @@ test('can create a event', async ({ client }) => {
       start: '2020-02-02 00:00:00',
       end: '2020-02-02 00:00:00',
     })
+    .loginVia(user, 'jwt')
     .end()
   response.assertStatus(201)
 })
 
-test('can delete event', async ({ client, assert }) => {
+test('unauthorized user can not create a event', async ({ client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  user.delete()
+  const response = await client
+    .post('/events')
+    .send({
+      owner: 'teste',
+      title: 'teste',
+      email: 'teste@gmail.com',
+      description: 'teste',
+      start: '2020-02-02 00:00:00',
+      end: '2020-02-02 00:00:00',
+    })
+    .loginVia(user, 'jwt')
+    .end()
+  response.assertStatus(401)
+})
+
+test('authorized user can delete event', async ({ client, assert }) => {
+  const user = await Factory.model('App/Models/User').create()
   const event = await Factory.model('App/Models/Event').create()
 
   const response = await client
     .delete(event.url())
     .send()
+    .loginVia(user, 'jwt')
     .end()
   response.assertStatus(204)
-  assert.equal(await Event.getCount(), 0)
+})
+
+test('unauthorized user can not delete event', async ({ client, assert }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const event = await Factory.model('App/Models/Event').create()
+  user.delete()
+  const response = await client
+    .delete(event.url())
+    .send()
+    .loginVia(user, 'jwt')
+    .end()
+  response.assertStatus(401)
 })
