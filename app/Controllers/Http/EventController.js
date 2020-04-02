672831b5c4c8594415fe99399d5708268efba7f8
quaresmateo <teoian.giovane@gmail.com>
2020-04-02 13:34:59 -0300

    Cria index do controller de eventos

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -10,7 +10,12 @@ const Event = use('App/Models/Event')
  * Resourceful controller for interacting with events
  */
 class EventController {
-  async index({ request, response, view }) {}
+  async index({ request, response }) {
+    let { page } = request.all()
+    page = page ? page : 1
+    const events = await Event.query().paginate(page ? page : 1, 10)
+    return response.json({ events })
+  }
 
   async store({ request, response }) {
     const event = await Event.create({
