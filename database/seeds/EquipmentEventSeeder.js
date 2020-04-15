ef55f3ce2485160be1513cd54b110dfe592bf046
quaresmateo <teoian.giovane@gmail.com>
2020-04-15 14:34:36 -0300

    Cria seeder para tabela pivot

---
+++ database/seeds/EquipmentEventSeeder.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| EquipmentEventSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class EquipmentEventSeeder {
+  async run() {
+    await Factory.get('equipment_event').createMany(5)
+  }
+}
+
+module.exports = EquipmentEventSeeder
