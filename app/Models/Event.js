179bd0106c357e93a8dcd52fa69e39441dd029fc
quaresmateo <teoian.giovane@gmail.com>
2020-06-30 09:30:18 -0300

    Relaciona horários com eventos

--- app/Models/Event.js
+++ app/Models/Event.js
@@ -14,6 +14,12 @@ class Event extends Model {
     )
   }
 
+  schedules() {
+    return this.belongsToMany('App/Models/Schedule').pivotTable(
+      'event_schedule'
+    )
+  }
+
   static get dates() {
     return super.dates.concat(['start', 'end'])
   }
