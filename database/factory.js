c056c33dc6123b8f18930593c1dd34858e260f21
quaresmateo <teoian.giovane@gmail.com>
2020-07-01 20:55:24 -0300

    Factory de dias desativados com horários

--- database/factory.js
+++ database/factory.js
@@ -50,19 +50,30 @@ Factory.blueprint('App/Models/Equipment', (faker) => {
 })
 
 Factory.blueprint('App/Models/DisableDay', async (faker) => {
+  const user = await Factory.model('App/Models/User').create()
   const now = new Date()
   const thisYear = now.getFullYear()
   const month = Math.random() * (11 - 0) + 0
   const day = Math.random() * (27 - 1) + 1
   const date = new Date(thisYear, month, day)
-
-  const user = await Factory.model('App/Models/User').create()
+  const full_day = Math.random() < 0.5
 
   return {
     title: faker.word(),
     description: faker.sentence({ words: 10 }),
+    full_day,
     date,
     user_id: user.id,
+  }
+})
+
+Factory.blueprint('disable_day_schedule', async (faker) => {
+  const schedule = await Factory.model('App/Models/Schedule').create()
+  const disableDay = await Factory.model('App/Models/DisableDay').create()
+
+  return {
+    schedule_id: schedule.id,
+    disable_day_id: disableDay.id,
   }
 })
 
