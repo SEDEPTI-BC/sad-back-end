d0f92da74d0abc5a2ac9a70b9611be50ab895706
quaresmateo <teoian.giovane@gmail.com>
2020-07-05 08:59:34 -0300

    Configura email provider

--- start/app.js
+++ start/app.js
@@ -11,11 +11,12 @@
 |
 */
 const providers = [
-    '@adonisjs/framework/providers/AppProvider',
-    '@adonisjs/auth/providers/AuthProvider',
-    '@adonisjs/bodyparser/providers/BodyParserProvider',
-    '@adonisjs/cors/providers/CorsProvider',
-    '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/framework/providers/AppProvider',
+  '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/bodyparser/providers/BodyParserProvider',
+  '@adonisjs/cors/providers/CorsProvider',
+  '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/mail/providers/MailProvider',
 ]
 
 /*
@@ -28,8 +29,8 @@ const providers = [
 |
 */
 const aceProviders = [
-    '@adonisjs/lucid/providers/MigrationsProvider',
-    '@adonisjs/vow/providers/VowProvider',
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  '@adonisjs/vow/providers/VowProvider',
 ]
 
 /*
